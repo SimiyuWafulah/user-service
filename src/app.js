077ed63e1cfaf883854c6e53caab6874120da8c7
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-07-25 14:18:25 +0300

    imports userRouter

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
 dotenv.config()
 
 const app = express();
 app.use(express.json());
+
 
 //connecting to database
 mongoose.connect(process.env.mongo_url).then(() => {
@@ -13,4 +15,5 @@ mongoose.connect(process.env.mongo_url).then(() => {
     console.log(err)
 });
 
-export default app
+app.use('/api/user', userRouter);
+export default app;
